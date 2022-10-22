c144550432a83038295f51c3d24732d81f780b45
Eric Priemer <elpanchopriemer@hotmail.com>
2022-10-22 10:30:15 -0300

    Display mobile menu

--- scripts.js
+++ scripts.js
@@ -7,3 +7,12 @@ emailNav.addEventListener('click',toggleMenu);
 function toggleMenu(){
     accMenu.classList.toggle('inactive')
 }
+
+var menuMob=document.querySelector(".menu");
+
+var mobMenu=document.querySelector(".mobile-menu");
+menuMob.addEventListener('click',toggleMenuMob);
+
+function toggleMenuMob(){
+    mobMenu.classList.toggle('inactive')
+}
